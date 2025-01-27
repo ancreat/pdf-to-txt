107e6ff5089bba97619940b0eef384e1ef2809de
ancreat <160452468+ancreat@users.noreply.github.com>
2025-01-27 22:51:41 +1100

    feat: add result

---
+++ src/store/file-history-store.ts
@@ -0,0 +1,29 @@
+import { create } from "zustand";
+
+interface FileHistory {
+  textFileName: string | null;
+  text: string | null;
+  isSuccess: boolean;
+  timestamp: string;
+}
+
+interface FileHistoryStore {
+  fileHistory: FileHistory[];
+  resetFileHistory: () => void;
+  appendFileHistory: (newFileHistory: Omit<FileHistory, "timestamp">) => void;
+}
+
+export const useFileHistoryStore = create<FileHistoryStore>((set) => ({
+  fileHistory: [],
+  resetFileHistory: () => set({ fileHistory: [] }),
+  appendFileHistory: (newFileHistory) =>
+    set((state) => ({
+      fileHistory: [
+        ...state.fileHistory,
+        {
+          ...newFileHistory,
+          timestamp: new Date().toLocaleString(),
+        },
+      ],
+    })),
+}));
