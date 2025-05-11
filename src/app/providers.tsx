12e1145ec9de33d6ec19f5a2e688a0a42a467933
ancreat <160452468+ancreat@users.noreply.github.com>
2025-05-11 20:59:26 +1000

    feat: remove type declaration

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,14 +3,6 @@
 import { HeroUIProvider } from "@heroui/react";
 import { useRouter } from "next/navigation";
 
-declare module "@react-types/shared" {
-  interface RouterConfig {
-    routerOptions: NonNullable<
-      Parameters<ReturnType<typeof useRouter>["push"]>[1]
-    >;
-  }
-}
-
 export function Providers({ children }: { children: React.ReactNode }) {
   const router = useRouter();
 
